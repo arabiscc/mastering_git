73a1d32cc368d7248b776a9d1a2aa481c5113862
arabiscc <sajabaaj11@gmail.com>
2025-09-23 17:41:18 +0300

    add revert console log

--- hello.js
+++ hello.js
@@ -1,2 +1,6 @@
-console.log("Hello, Git");
-console.log("Hello, GitHub!");
+console.log('Hello, Git');
+console.log('Hello, GitHub!');
+console.log('Hello, dev-adrian');
+console.log('This is bad code');
+console.log('Trying out revert');
+
